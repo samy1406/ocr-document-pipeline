2ab0fadee78a87734f1b82b7685777430d0618cb
Samarth <32644389+samy1406@users.noreply.github.com>
2026-05-06 07:58:05 +0000

    add the code for the ocr engine

---
+++ src/ocr_engine.py
@@ -0,0 +1,22 @@
+import pytesseract
+from pdf2image import convert_from_path
+
+def image_to_text(image):
+    text = pytesseract.image_to_string(image, config='--psm 6')
+    return text
+
+def image_to_data(image):
+    df = pytesseract.image_to_data(image, output_type=pytesseract.Output.DATAFRAME)
+    df = df[(df['conf'] != -1) & (df['conf'] > 60)]
+    return df
+
+def pdf_to_text(pdf_path):
+    pages = convert_from_path(pdf_path, dpi=300)
+    
+    text_pages = []
+
+    for page in pages:
+        page_text = image_to_text(page)
+        text_pages.append(page_text)
+    
+    return "\n\n".join(text_pages)
